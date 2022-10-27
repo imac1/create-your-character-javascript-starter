63afc2adb37ed3ae3861654bc884a49a25cf1586
imac1 <irina.macovei@yahoo.fr>
2022-10-27 19:43:12 +0300

    final

--- script.js
+++ script.js
@@ -10,9 +10,39 @@ function genderChange() {
 	let genderFormSelector = document.getElementById("gender");
 	let genderImg = document.getElementById("gender-img");
 	genderFormSelector.addEventListener("change", (e) => {
-		genderImg.src = e.target.value + ".png";
+		genderImg.src =
+			"../create-your-character-javascript-starter/media/gender/" +
+			e.target.value +
+			".png";
 		console.log(genderImg.src);
 	});
 }
+
+function raceChange() {
+	let genderFormSelector = document.getElementById("race");
+	let genderImg = document.getElementById("race-img");
+	genderFormSelector.addEventListener("change", (e) => {
+		genderImg.src =
+			"../create-your-character-javascript-starter/media/race/" +
+			e.target.value +
+			".png";
+		console.log(genderImg.src);
+	});
+}
+
+function classChange() {
+	let genderFormSelector = document.getElementById("class");
+	let genderImg = document.getElementById("class-img");
+	genderFormSelector.addEventListener("change", (e) => {
+		genderImg.src =
+			"../create-your-character-javascript-starter/media/class/" +
+			e.target.value +
+			".png";
+		console.log(genderImg.src);
+	});
+}
+
 window.addEventListener("load", formInput);
 window.addEventListener("change", genderChange);
+window.addEventListener("change", raceChange);
+window.addEventListener("change", classChange);
